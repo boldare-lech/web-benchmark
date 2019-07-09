b8dfbbf65a6b083254d6c7e32bc86f897ec2f55f
Lech <lech.sawon@boldare.com>
2019-07-09 10:20:18 +0200

    php unit installation

---
+++ tests/Functional/Command/WebBenchmarkCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests\Functional\Command;
+
+use PHPUnit\Framework\TestCase;
+
+
+class WebBenchmarkCommandTest extends TestCase
+{
+    public function testCommand() {
+
+    }
+
+}
